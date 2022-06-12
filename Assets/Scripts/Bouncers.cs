8dde44748baa5666d7b78c31cd79ead5e88efedd
leviljv <levivermaning@gmail.com>
2022-06-12 15:27:29 +0200

    Look at player when grabbing keycard (broken for now)

--- Assets/Scripts/Bouncers.cs
+++ Assets/Scripts/Bouncers.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class Bouncers : MonoBehaviour
 {
+    public GameObject[] humans;
+    public Transform player;
     public Collider col;
     public Animator anim1, anim2;
     public bool allowed = false;
+
+    public bool snapToPlayer = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,14 +20,41 @@ public class Bouncers : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(snapToPlayer)
+        {
+            foreach (GameObject human in humans)
+            {
+                Vector3 point = player.position;
+                point.y = 0;
+                Transform NPCS = human.GetComponent<Transform>();
+                NPCS.LookAt(Vector3.Lerp(NPCS.position, player.position, 2));
+
+                Animator anim = human.GetComponent<Animator>();
+                anim.SetTrigger("Look");
+               
+                
+                
+                
+
+                    
+            }
+        }
         
     }
-
+    
     public void AllowEntrance()
     {
         col.enabled = false;
+        snapToPlayer = true;
+        
     }
-
+    public void TurnToPlayer(GameObject human)
+    {
+        Vector3 point = player.position;
+        point.y = 0;
+        Transform NPCS = human.GetComponent<Transform>();
+        NPCS.LookAt(point);
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (!allowed) {
